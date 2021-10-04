283b492ea89da64728ba7921e0b27e986bb7cf54
Nautilus Vanuatu <nautilus.vanuatu@gmail.com>
2021-10-04 12:27:59 -0300

    feat: Add menu items/subitems for desktop and mobile

---
+++ packages/vote-web-admin/components/layout/Navigation/DesktopSubNav/index.tsx
@@ -0,0 +1,52 @@
+import {
+  Box,
+  Flex,
+  Text,
+  Stack,
+  Icon,
+  Link,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+import { ChevronRightIcon } from '@chakra-ui/icons';
+
+import { NavItem } from '../items';
+
+const DesktopSubNav = ({ label, href, subLabel }: NavItem): JSX.Element => {
+  return (
+    <Link
+      href={href}
+      role="group"
+      display="block"
+      p={2}
+      rounded="md"
+      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
+    >
+      <Stack direction="row" align="center">
+        <Box>
+          <Text
+            transition="all .3s ease"
+            _groupHover={{ color: 'pink.400' }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize="sm">{subLabel}</Text>
+        </Box>
+        <Flex
+          transition="all .3s ease"
+          transform="translateX(-10px)"
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify="flex-end"
+          align="center"
+          flex={1}
+        >
+          <Icon color="pink.400" w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  );
+};
+
+export default DesktopSubNav;
