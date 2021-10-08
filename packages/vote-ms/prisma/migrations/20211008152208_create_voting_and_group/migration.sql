85f6a56ffd76b34a2b773bfb3772ea2014682752
Nautilus Vanuatu <nautilus.vanuatu@gmail.com>
2021-10-08 12:37:49 -0300

    backend/refactor: Update schema

--- packages/vote-ms/prisma/migrations/20211008152208_create_voting_and_group/migration.sql
+++ packages/vote-ms/prisma/migrations/20211008152208_create_voting_and_group/migration.sql
@@ -1,21 +1,16 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('OPEN', 'CLOSED', 'REGISTERED');
 
 -- CreateTable
-CREATE TABLE "Vote" (
+CREATE TABLE "Voting" (
     "id" TEXT NOT NULL,
+    "id_system" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "desc" TEXT NOT NULL,
     "begin_date" TIMESTAMP(3) NOT NULL,
     "end_date" TIMESTAMP(3) NOT NULL,
     "groupId" TEXT NOT NULL,
-    "Status" CHAR(1) NOT NULL,
+    "Status" "Status" NOT NULL DEFAULT E'REGISTERED',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -30,5 +25,8 @@ CREATE TABLE "Group" (
     PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Voting.id_system_unique" ON "Voting"("id_system");
+
 -- AddForeignKey
-ALTER TABLE "Vote" ADD FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Voting" ADD FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
